a6c582376b2687e26f51c61cc1bc8d330f3bacb9
Andrejs Filimonovs <andrejs.filimonovs@gmail.com>
2026-06-07 21:13:32 +0300

    Add new topic page has been created.

--- blogs/blogs_app/views.py
+++ blogs/blogs_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """The home page for Blogs app."""
@@ -20,3 +21,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'blogs_app/topic.html',context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method!='POST':
+        # No data submitted;create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted;process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs_app:topics')
+        
+    # Display a blank or invalid form.
+    context = {'form':form}
+    return render(request,'blogs_app/new_topic.html',context)
