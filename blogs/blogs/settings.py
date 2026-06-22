b1a9ef0560a5fce08ff97aea0475cd422e48a838
Andrejs Filimonovs <andrejs.filimonovs@gmail.com>
2026-06-22 17:33:17 +0300

    FInal version of blogs app project ready to be published on platformsh.

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'blogs_app',
     'accounts',
     
+    # Third party apps.
+    'django_bootstrap5',
+    
     # Default django apps 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +127,30 @@ STATIC_URL = 'static/'
 
 # My settings.
 LOGIN_REDIRECT_URL = 'blogs_app:index'
+LOGOUT_REDIRECT_URL = 'blogs_app:index'
+LOGIN_URL = 'accounts:login'
+
+# Platform.sh settings.
+from platformshconfig import Config
+
+config = Config()
+
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+    
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir)/'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+    if not config.in_build():
+        db_settings = config.credentials('database')
+        DATABASES = {
+            'default':{
+                'ENGINE':'django.db.backends.postgresql',
+                'NAME':db_settings['path'],
+                'USER':db_settings['username'],
+                'PASSWORD':db_settings['password'],
+                'HOST':db_settings['host'],
+                'PORT':db_settings['port'],
+            }
+        }
--- blogs/blogs_app/views.py
+++ blogs/blogs_app/views.py
@@ -1,27 +1,37 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
 from .models import Topic,Entry
 from .forms import TopicForm,EntryForm
 
+
 def index(request):
     """The home page for Blogs app."""
     return render(request, 'blogs_app/index.html')
 
+@login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    # topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request,'blogs_app/topics.html',context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner !=request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'blogs_app/topic.html',context)
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method!='POST':
@@ -31,6 +41,9 @@ def new_topic(request):
         # POST data submitted;process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             form.save()
             return redirect('blogs_app:topics')
         
@@ -38,6 +51,7 @@ def new_topic(request):
     context = {'form':form}
     return render(request,'blogs_app/new_topic.html',context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -58,10 +72,13 @@ def new_entry(request, topic_id):
     context = {'topic':topic,'form':form}
     return render(request, 'blogs_app/new_entry.html',context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     
     if request.method !='POST':
         # Initial request;pre-fill form with the current entry.
